3bf763e79c700baa3dc3a30448cc2f93e7f241db
Joy <github.joysaha@gmail.com>
2024-03-01 12:35:09 +0600

    video card modified

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const btnContainer = document.getElementById('btn-container');
+const cardContainer = document.getElementById('card-container')
+
+let selectedCategory = 1000;
 
 const fetchCategories = () => {
     const url = 'https://openapi.programming-hero.com/api/videos/categories';
@@ -17,13 +20,43 @@ const fetchCategories = () => {
 }
 
 const fetchDataByCategories = (categoryID) => {
+    selectedCategory = categoryID;
     const url = `https://openapi.programming-hero.com/api/videos/category/${categoryID}`;
     fetch(url)
     .then((res) => res.json())
     .then(({data}) => {
-        
+        cardContainer.innerHTML = ''
+        data.forEach((video) => {
+            
+            const newCard = document.createElement('div')
+            newCard.innerHTML = `
+            <div class="card w-full bg-base-100 shadow-xl">
+            <figure class="overflow-hidden h-72">
+                <img class="w-full" src="${video.thumbnail}" alt="image" />
+                <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
+            </figure>
+            <div class="card-body">
+                <div class="flex space-x-4 justify-start items-start">
+                    <div>
+                        <img class="w-12 h-12 rounded-full" src="${video.authors[0].profile_picture}" alt="Shoes" />
+                    </div>
+                    <div>
+                        <h2 class="card-title">${video.title}</h2>
+                        <div class="flex mt-3">
+                            <p class="">${video.authors[0].profile_name}</p>
+                            <img class="w-6 h-6" src="./images/verify.png" alt="">
+                        </div>
+                        <p class="mt-3">${video.others.views}</p>
+                    </div>
+                </div>
+            </div>
+         </div>
+            `;
+            cardContainer.appendChild(newCard)
+        })
     })
 }
 
 
 fetchCategories();
+fetchDataByCategories(selectedCategory);

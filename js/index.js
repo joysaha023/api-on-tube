58afc0b5addb6965f7928cac07242f5a4f1f86df
Joy <github.joysaha@gmail.com>
2024-03-01 11:42:45 +0600

    button and fetch data functionality added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("joy saha")
+const btnContainer = document.getElementById('btn-container');
+
+const fetchCategories = () => {
+    const url = 'https://openapi.programming-hero.com/api/videos/categories';
+    fetch(url)
+        .then((res) => res.json())
+        .then(({data}) => {
+            data.forEach((card) => {
+                console.log(card)
+                const newBtn = document.createElement('button')
+                newBtn.className = 'btn btn-ghost bg-slate-700 text-white text-lg';
+                newBtn.innerText = card.category
+                newBtn.addEventListener('click', () => fetchDataByCategories(card.category_id))
+                btnContainer.appendChild(newBtn)
+            })
+        })
+}
+
+const fetchDataByCategories = (categoryID) => {
+    console.log(categoryID)
+}
+
+
+fetchCategories();
